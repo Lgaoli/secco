be8d09f2f00f878ca5c1e147b3bfdfc312ac27d7
malin <2318447107@qq.com>
2019-03-26 13:44:17 +0800

    1

--- dist/api/list.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connc.php';
-
-
-$page = isset($_POST['page']) ? $_POST['page'] : '';// 当前页数
-$num = isset($_POST['num']) ? $_POST['num'] : '';//每页显示得条数
-
-
- 
--- dist/api/shop.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connc.php';
-
-$sql="SELECT * FROM shop";
-
-$cons=$con->query($sql);
-$arr = $cons->fetch_all(MYSQLI_ASSOC);
-
-echo json_encode($arr,JSON_UNESCAPED_UNICODE);
